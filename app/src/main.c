81787291bef3192c11600311331348c47af969d3
phuc <phuc@mijoconnected.com>
2026-02-17 21:58:37 +0700

    led driver complete

--- app/src/main.c
+++ app/src/main.c
@@ -11,17 +11,34 @@
 #include "app.h"
 #include "ble.h"
 #include "buttons.h"
+#include "led.h"
 #include "power.h"
 #include "rtc.h"
 
 LOG_MODULE_REGISTER(main);
+
+static void led_test(void) {
+  LOG_INF("Testing LEDs...");
+  for (int i = 0; i < 3; ++i) {
+    led_set_color(0x20, 0x00, 0x00); /* red */
+    k_msleep(200);
+    led_set_color(0x00, 0x20, 0x00); /* green */
+    k_msleep(200);
+    led_set_color(0x00, 0x00, 0x20); /* blue */
+    k_msleep(200);
+  }
+  led_set_color(0x00, 0x00, 0x00); /* off */
+}
 
 int main(void) {
   LOG_INF("K Watch starting");
 
   buttons_init();
   rtc_init();
+  led_init();
   power_init();
+
+  led_test();
 
   if (ble_init() < 0) {
     LOG_ERR("BLE init failed");
